7bb49ea04e301300c94c6f295b085fdbe2a0c740
esevre <esevre@gmail.com>
2020-09-11 15:10:41 +0900

    added examples to project 2, and some comments to project 2. Started looking at using vector objects.

--- 2020-09-11/Project02/main.cpp
+++ 2020-09-11/Project02/main.cpp
@@ -4,25 +4,26 @@
 
 #include <iostream>
 #include <complex>
+#include <vector>
 
 template <class Type>
 class Point {
 public:
-    Point(Type x, Type y) : x(x), y(y) {
-//        Point::x = x;  // Class Resolution Operator
-//        this->y = y;   // this pointer to access members
-    }
+    Point(Type x, Type y) : x(x), y(y) {}
 public:
     Type x{};
     Type y{};
 };
 
+
+// generic function to print Point objects
 template <class Type>
 void print(const Point<Type> &point)
 {
     std::cout << "(" << point.x << ", " << point.y << ")\n";
 }
 
+// use a template specialization to print a complex point with the formatting I like
 template <class ComplexType>
 void print(const Point<std::complex<ComplexType>> &point) {
     std::cout << "("
@@ -33,5 +34,17 @@ void print(const Point<std::complex<ComplexType>> &point) {
 
 int main()
 {
+    std::vector<Point<int>> int_points;
+    const int vec_size = 10;
+    int_points.reserve(vec_size*2);
+    for (int i = 0; i < vec_size; ++i) {
+        // pushes an object onto the end of a vector
+        int_points.push_back( Point<int>(i, 2*i) );
+        // creates an object with given parameters at the end of a vector
+        int_points.emplace_back(i, 2*i);
+    }
+    for (const auto &point : int_points) {
+        print(point);
+    }
 
 }
