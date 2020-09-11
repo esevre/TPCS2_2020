3c83a33cb35062a98b3b4758374395da1d2aa3a5
esevre <esevre@gmail.com>
2020-09-11 14:40:32 +0900

    Added point object, and print function for points.

--- 2020-09-11/Project01/main.cpp
+++ 2020-09-11/Project01/main.cpp
@@ -2,6 +2,29 @@
 // Created by esevre on 9/11/2020.
 //
 
+#include <iostream>
+
+template <class Type>
+class Point {
+public:
+    Point(Type x, Type y) : x(x), y(y) {
+//        Point::x = x;  // Class Resolution Operator
+//        this->y = y;   // this pointer to access members
+    }
+public:
+    Type x{};
+    Type y{};
+};
+
+template <class Type>
+void print(const Point<Type> &point)
+{
+    std::cout << "(" << point.x << ", " << point.y << ")\n";
+}
+
+
 int main(){
-    return 42;
+    Point<int> p{1,2};
+    print(p);
+    return p.x;
 }
